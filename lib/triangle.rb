5ea2391f20cc3766a96681682ffc1ca01395116e
kiburei <akiburei@gmail.com>
2017-07-03 11:48:44 +0300

    create class triangle and accept three args

---
+++ lib/triangle.rb
@@ -0,0 +1,11 @@
+class Triangle
+    define_method(:initialize) do |a, b, c|
+        @a = a
+        @b = b
+        @c = c
+    end
+
+    define_method(:check) do
+          a = @a      
+    end
+end
---
+++ spec/triangle_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('triangle')
+
+describe('Triangle#triangle') do
+    it('expect three inputs')do
+        expect(1.triangle).to(eq(1))
+    end
+end
